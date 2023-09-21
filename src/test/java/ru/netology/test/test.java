62f86f4facf006190636aca70bcd4b578695e6f1
Netology <orjuice@mail.ru>
2023-09-21 20:14:05 +0300

    Final commit

--- src/test/java/ru/netology/test/test.java
+++ src/test/java/ru/netology/test/test.java
@@ -9,23 +9,20 @@ import ru.netology.page.LoginPage;
 import ru.netology.page.VerificationPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanDataBase;
 
 public class test {
 
     @AfterAll
         static void teardown(){
-       SQLHelper.cleanDataBase();
-    }
-    @BeforeEach
-    void setup() {
-        open("http://localhost:9999");
-        LoginPage loginPage = new LoginPage();
+       cleanDataBase();
     }
 
     @Test
     public void shouldLoginSuccessfully() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
         var auth_info = DataHelper.getAuthInfoValid();// берем инфо о валидном пользователе
-        var verificationPage = LoginPage.validLogin();// проходим процедуру вставки валидных логина и пароля и отправки данных, появляется страница верификации, где надо ввести код
+        var verificationPage = LoginPage.validLogin(authInfo);// проходим процедуру вставки валидных логина и пароля и отправки данных, появляется страница верификации, где надо ввести код
         verificationPage.verifyVerificationPageVisibility();// проверяем, что появилась страница верификации
         verificationPage.validVerify(DataHelper.code);//вставка валидного кода - 12345
     }
